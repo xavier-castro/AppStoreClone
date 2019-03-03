afb53d61e94a883cfcfb81117f7ee13aa7f02bf6
xavier-castro <xavier_castro@icloud.com>
2019-03-02 23:34:01 -0600

    Refactor viewControllers()

---
+++ App-Store-Mockup/Controller/AppsSearchController.swift
@@ -0,0 +1,24 @@
+//
+//  AppsSearchController.swift
+//  App-Store-Mockup
+//
+//  Created by Xavier Castro on 3/2/19.
+//  Copyright © 2019 Xavier Castro. All rights reserved.
+//
+
+import UIKit
+
+class AppsSearchController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+    }
+
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ App-Store-Mockup/Controller/BaseTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  BaseTabBarController.swift
+//  App-Store-Mockup
+//
+//  Created by Xavier Castro on 3/2/19.
+//  Copyright © 2019 Xavier Castro. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+
+    // 3 - Introduce AppsSearchController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+            createNavController(viewController: AppsSearchController(), title: "Search", imageName: "search")
+        ]
+    }
+
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .white
+        return navController
+    }
+
+}
