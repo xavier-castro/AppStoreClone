82581215941ca70b36aa1de82f31526cc8ba13e5
xavier-castro <xavier_castro@icloud.com>
2019-03-02 22:49:00 -0600

    Initial Commit

---
+++ App-Store-Mockup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App-Store-Mockup
+//
+//  Created by Xavier Castro on 3/2/19.
+//  Copyright © 2019 Xavier Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
