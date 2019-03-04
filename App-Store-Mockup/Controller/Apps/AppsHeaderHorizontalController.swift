3e50a779c7ad0f45d0cfd6d2bdc95d61cc237487
xavier-castro <xavier_castro@icloud.com>
2019-03-04 16:48:56 -0600

    Add company, title, and picture to AppHorizontalHeader

--- App-Store-Mockup/Controller/Apps/AppsHeaderHorizontalController.swift
+++ App-Store-Mockup/Controller/Apps/AppsHeaderHorizontalController.swift
@@ -14,7 +14,7 @@ class AppsHeaderHorizontalController: BaseListController, UICollectionViewDelega
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .yellow
+        collectionView.backgroundColor = .white
         collectionView.register(AppsHeaderCell.self, forCellWithReuseIdentifier: cellId)
 
         if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
@@ -29,11 +29,15 @@ class AppsHeaderHorizontalController: BaseListController, UICollectionViewDelega
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .red
         return cell
     }
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: view.frame.height)
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
     }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width - 48, height: view.frame.height)
+    }
+
 }
--- App-Store-Mockup/Views/Apps/AppsHeaderCell.swift
+++ App-Store-Mockup/Views/Apps/AppsHeaderCell.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class AppsHeaderCell: UICollectionViewCell {
+
+    let companyLabel = UILabel(text: "Facebook", font: .boldSystemFont(ofSize: 12))
+    let titleLabel = UILabel(text: "Keeping up with friends is faster than ever", font: .systemFont(ofSize: 24))
+    let imageView = UIImageView(cornerRadius: 8)
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .green
+        companyLabel.textColor = .blue
+        imageView.backgroundColor = .red
+        titleLabel.numberOfLines = 2
+        let stackView = VerticalStackView(arrangedSubviews: [companyLabel, titleLabel, imageView], spacing: 12)
+        addSubview(stackView)
+        stackView.fillSuperview(padding: UIEdgeInsets(top: 16, left: 0, bottom: 0, right: 0))
     }
 
     required init?(coder aDecoder: NSCoder) {
