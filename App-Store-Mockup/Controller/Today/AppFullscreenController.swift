c75844e8c4ff7db6a26a4b788c9b23dc57a00752
xavier-castro <xavier_castro@icloud.com>
2019-03-07 02:14:45 -0600

    Fix duplicate glitch

--- App-Store-Mockup/Controller/Today/AppFullscreenController.swift
+++ App-Store-Mockup/Controller/Today/AppFullscreenController.swift
@@ -24,7 +24,7 @@ class AppFullscreenController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
