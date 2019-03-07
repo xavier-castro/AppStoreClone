36454243ef4ebdcefb2ca0cdc0d3352ea6e48734
xavier-castro <xavier_castro@icloud.com>
2019-03-07 02:21:08 -0600

    Add top padding to description label

--- App-Store-Mockup/Views/Today/AppFullscreenDescriptionCell.swift
+++ App-Store-Mockup/Views/Today/AppFullscreenDescriptionCell.swift
@@ -37,7 +37,7 @@ class AppFullscreenDescriptionCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
         addSubview(descriptionLabel)
-        descriptionLabel.fillSuperview(padding: .init(top: 0, left: 24, bottom: 0, right: 24))
+        descriptionLabel.fillSuperview(padding: .init(top: 24, left: 24, bottom: 0, right: 24))
     }
 
     required init?(coder aDecoder: NSCoder) {
